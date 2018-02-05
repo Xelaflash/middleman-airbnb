f25e03eeb07615060bf54bfb0cd28e42776fb5cc
Alexandre Gros <xelaflash@gmail.com>
2018-02-05 18:01:48 +0100

    flat page css to do

--- config.rb
+++ config.rb
@@ -20,3 +20,8 @@ activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
 end
+
+
+["ClaudineP435433","VictorSass", "Xelaflash", "Smabi", "alixdelb", "DamienBoussardon"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", locals: { owner: name }, ignore: true
+end
